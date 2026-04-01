dc5ae2e47ff61cb6bd0cad6616a8cb97dc22322f
Onebluesky882 <onebluesky882@outlook.com>
2026-04-01 12:44:19 +0700

    build : redis

--- main.go
+++ main.go
@@ -1,10 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/redis/go-redis/v9"
+)
 
 func main() {
-	fmt.Println("hello")
-	fmt.Println("hello")
-	fmt.Println("hello")
-	fmt.Println("hello")
+	ctx := context.Background()
+	rdb := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+	// test
+	err := rdb.Set(ctx, "key", "hello2", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+	val, err := rdb.Get(ctx, "key").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("value:", val)
 }
